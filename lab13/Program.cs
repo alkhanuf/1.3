a7ca20eaaa1e386e447028dacc9b3920b42541f0
alkhanuf <simoon.phv@gmail.com>
2026-02-22 20:22:22 +0800

    ввод предложений и разделение

--- lab13/Program.cs
+++ lab13/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace lab13
+﻿//Даны два предложения. Для каждого слова первого предложения определить, входит ли оно во второе предложение.
+//Повторяющиеся слова первого предложения не рассматривать.
+
+namespace lab13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("введите первое предложение: ");
+            string s1 = Console.ReadLine();
+
+            Console.WriteLine("введите второе предложение: ");
+            string s2 = Console.ReadLine();
+        
+            string[] words1 = s1.Split(' ');
+            string[] words2 = s2.Split(' ');
+
+            Console.WriteLine(string.Join(", ", words1));
+            Console.WriteLine(string.Join(", ", words2));
         }
     }
 }
