69fd54a76f7b45df4a21915c4da06de92f1cc01e
alkhanuf <simoon.phv@gmail.com>
2026-02-24 22:05:25 +0800

    декомпозиция

---
+++ lab13/Logic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Logic
+{
+    public static string[] FindWords(string s1, string s2)
+    {        
+        string[] words1 = s1.Split(' ');
+        string[] words2 = s2.Split(' ');
+
+        string[] res = new string[words1.Length];
+
+        int resi = 0;
+
+        for (int i = 0; i < words1.Length; i++)
+        {
+            for (int j = 0; j < words2.Length; j++)
+            {
+                if (words1[i] == words2[j])
+                {
+                    bool f = false;
+
+                    for (int k = 0; k < res.Length; k++)
+                    {
+                        if (res[k] == words1[i])
+                        {
+                            f = true;
+                            break;
+                        }
+                    }
+
+                    if (f == false)
+                    {
+                        res[resi] = words1[i];
+                        resi++;
+                    }
+                }
+            }
+        }
+
+        string[] finalres = new string[resi];
+
+        for (int i = 0; i < resi; i++)
+        {
+            finalres[i] = res[i];
+        }
+
+        return finalres;
+    }
+}
--- lab13/Program.cs
+++ lab13/Program.cs
@@ -1,7 +1,4 @@
-﻿//Даны два предложения. Для каждого слова первого предложения определить, входит ли оно во второе предложение.
-//Повторяющиеся слова первого предложения не рассматривать.
-
-namespace lab13
+﻿namespace lab13
 {
     internal class Program
     {
@@ -12,52 +9,10 @@ namespace lab13
 
             Console.WriteLine("введите второе предложение: ");
             string s2 = Console.ReadLine();
-        
-            string[] words1 = s1.Split(' ');
-            string[] words2 = s2.Split(' ');
 
-            string[] res = new string[words1.Length];
+            string[] res = Logic.FindWords(s1, s2);
 
-            int resi = 0;
-
-            //Console.WriteLine(string.Join(", ", words1));
-            //Console.WriteLine(string.Join(", ", words2));
-
-            for (int i = 0; i < words1.Length; i++)
-            {
-                for (int j = 0; j < words2.Length; j++)
-                {
-                    if (words1[i] == words2[j])
-                    {
-                        bool f = false;
-
-                        for (int k = 0; k < res.Length; k++)
-                        {
-                            if (res[k] == words1[i])
-                            {
-                                f = true; 
-                                break; 
-                            }
-                        }
-
-                        if (f == false)
-                        {
-                            res[resi] = words1[i];
-                            resi++;
-                        }
-                        
-                    }
-                }
-            }
-
-            string[] finalres = new string[resi];
-
-            for (int i = 0; i < resi; i++)
-            {
-                finalres[i] = res[i];
-            }
-
-            Console.WriteLine(string.Join(", ", finalres));
+            Console.WriteLine(string.Join(", ", res));
         }
     }
 }
